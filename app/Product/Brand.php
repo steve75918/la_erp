85ca9fc5cd2641ebbf117ad9d4b7fa3f204d75a9
steve.chen <steve.chen@papy.com.tw>
2018-07-13 00:54:20 +0800

    Create relationship of series and brands which is N:1

--- app/Product/Brand.php
+++ app/Product/Brand.php
@@ -8,6 +8,13 @@ class Brand extends Model
 {
     protected $connection = 'product';
 
+    /* 1:N */
+    public function series()
+    {
+        return $this->hasMany('App\Product\Series');
+    }
+
+    /* N:1 */
     public function publisher()
     {
         return $this->belongsTo('App\Product\Publisher');
--- app/Product/Series.php
+++ app/Product/Series.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Series extends Model
 {
     protected $connection = 'product';
+
+    /* N:1 */
+    public function brand()
+    {
+        return $this->belongsTo('App\Product\Brand');
+    }
 }
