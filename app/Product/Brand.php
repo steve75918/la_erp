cfb5b93bfd858202985a92ce90888c9aea69ed34
steve.chen <steve.chen@papy.com.tw>
2018-07-13 00:50:41 +0800

    Create relationship of brands and publishers which is N:1

--- app/Product/Brand.php
+++ app/Product/Brand.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Brand extends Model
 {
     protected $connection = 'product';
+
+    public function publisher()
+    {
+        return $this->belongsTo('App\Product\Publisher');
+    }
 }
--- app/Product/Publisher.php
+++ app/Product/Publisher.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Publisher extends Model
 {
     protected $connection = 'product';
+
+    public function brands()
+    {
+        return $this->hasMany('App\Product\Brand');
+    }
 }
