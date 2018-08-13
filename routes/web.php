b927dc3e0c0c8a369ce38df382122cb15206aa98
steve.chen <steve.chen@papy.com.tw>
2018-08-13 12:36:40 +0800

    Add authors:controller and route

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,5 @@ Route::prefix('product')->group(function () {
 
     Route::resource('publishers', 'Product\PublishersController');
     Route::resource('brands', 'Product\BrandsController');
+    Route::resource('authors', 'Product\AuthorsController');
 });
