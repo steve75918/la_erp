5de037aa44d8955112b0de329290a948b1f1698a
steve.chen <steve.chen@papy.com.tw>
2018-08-15 18:24:52 +0800

    Fix title error in categories.index

--- resources/views/product/categories/index.blade.php
+++ resources/views/product/categories/index.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
-            <h2>作者清單</h2>
+            <h2>作品類別清單</h2>
             <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
                 <a class="btn btn-sm btn-outline-secondary" href="{{ route('categories.create') }}">新增</a>
