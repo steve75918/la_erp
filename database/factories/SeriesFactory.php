5025e943ca7b9894a6b65613265bfd6e325047ac
steve.chen <steve.chen@papy.com.tw>
2018-08-17 16:27:41 +0800

    Remove author_id because it is a many to many relation.

--- database/factories/SeriesFactory.php
+++ database/factories/SeriesFactory.php
@@ -7,16 +7,12 @@ $factory->define(App\Product\Series::class, function (Faker $faker) {
     $fakerJP = \Faker\Factory::create('ja_JP');
 
     return [
-        'series_id' => $faker->unique()->randomNumber(5),
-        'author_id' => function () {
-            return factory(APP\Product\Author::class)->create()->id;
-        },
+        'series_id' => $faker->unique()->randomNumber(6),
         'brand_id' => function () {
             return factory(APP\Product\Brand::class)->create()->id;
         },
         'name' => $fakerTW->name,
         'origin_name' => $fakerJP->name,
         'desc' => $fakerTW->text,
-
     ];
 });
