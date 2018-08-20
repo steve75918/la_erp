1ba90f328c585cb37c64b452b0f2450890d22c6a
steve.chen <steve.chen@papy.com.tw>
2018-08-20 17:18:21 +0800

    Add many to many data in seeder.

--- database/seeds/SeriesTableSeeder.php
+++ database/seeds/SeriesTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Product\Author as Author;
+use App\Product\Category as Category;
+use App\Product\Tag as Tag;
 
 class SeriesTableSeeder extends Seeder
 {
@@ -11,7 +14,22 @@ class SeriesTableSeeder extends Seeder
      */
     public function run()
     {
-        $series = factory(App\Product\Series::class, 20)
-                    ->create();
+        $series = factory(App\Product\Series::class, 5)
+                    ->create()
+                    ->each(function ($s) {
+                        // set pivot table(many to many)
+                        $s->authors()->attach(
+                            Author::all()->random(rand(1, 2))->pluck('id')
+                        );
+
+                        // authought design as M:N, need only 1 now
+                        $s->categories()->attach(
+                            Category::all()->random(1)->pluck('id')
+                        );
+
+                        $s->tags()->attach(
+                            Tag::all()->random(5)->pluck('id')
+                        );
+                    });
     }
 }
