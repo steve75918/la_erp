e6711ebbee98c46a695e9305435ec48884d6eabb
steve.chen <steve.chen@papy.com.tw>
2018-08-24 13:44:29 +0800

    Add feature:Delete for books

--- app/Http/Controllers/Product/BookController.php
+++ app/Http/Controllers/Product/BookController.php
@@ -127,6 +127,9 @@ class BookController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $book = Book::find($id);
+        $book->delete();
+
+        return redirect()->route('books.index');
     }
 }
